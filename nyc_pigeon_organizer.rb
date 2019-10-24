ae47adb4b708405293611baad8e61fd7a51db9a3
Yining Wang <yining.wang@utexas.edu>
2019-10-24 03:24:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = {}
+  all_pigeons = data[:gender][:male] + data[:gender][:female]
+  all_pigeons.each{|pigeon| new_data[pigeon] = {}}
+  # data[:color].keys.each{|color| data[:color][color].each{|pigeon| new_data[pigeon][:color]? new_data[pigeon][:color].append(color.to_s) : new_data[pigeon][:color] = [color.to_s]}}
+  data.each{|cat, items| items.each{|item_name, pigeons_arr| pigeons_arr.each{|pigeon|
+  if new_data[pigeon][cat]
+    new_data[pigeon][cat].append(item_name.to_s)
+  elsif new_data[pigeon]
+    new_data[pigeon][cat] = [item_name.to_s]
+  end
+  }}}
+  new_data
 end
