d72e813d3402208efeddb8cee078889fec54ffdd
Yining Wang <yining.wang@utexas.edu>
2019-10-24 20:16:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,11 +4,7 @@ def nyc_pigeon_organizer(data)
   all_pigeons.each{|pigeon| new_data[pigeon] = {}}
   # data[:color].keys.each{|color| data[:color][color].each{|pigeon| new_data[pigeon][:color]? new_data[pigeon][:color].append(color.to_s) : new_data[pigeon][:color] = [color.to_s]}}
   data.each{|cat, items| items.each{|item_name, pigeons_arr| pigeons_arr.each{|pigeon|
-  if new_data[pigeon][cat]
-    new_data[pigeon][cat].append(item_name.to_s)
-  elsif new_data[pigeon]
-    new_data[pigeon][cat] = [item_name.to_s]
-  end
+
   }}}
   new_data
 end
